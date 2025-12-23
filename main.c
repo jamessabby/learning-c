31df5c640f27d8beda849f01dad4b120d063217b
James Sabio <jamesjacobsabio232@gmail.com>
2025-12-23 10:21:16 +0800

    Initial C setup and first program

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // This is my first program
+
+    printf("hello\n");
+    printf("I am James\n");
+
+    // variables
+
+    // integer
+    int age = 20;
+    int year = 3;
+
+    printf("I'm %d years old\n", age);
+    printf("I'm a computer engineering student year %d", year);
+
+    // float
+
+    // long float or doubles for high precision numbers
+
+    //character
+
+    return 0;
+}
