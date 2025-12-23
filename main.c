bece6230f8a2fdb87784dcb714970df1897404b0
James Sabio <jamesjacobsabio232@gmail.com>
2025-12-23 16:10:35 +0800

    Learned user input

--- main.c
+++ main.c
@@ -1,58 +1,117 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h> //provides useful functions related to string
 
 int main(void) {
 
-    // This is my first program
+    // // This is my first program
 
-    printf("hello\n");
-    printf("I am James\n");
+    // printf("hello\n");
+    // printf("I am James\n\n");
+    
 
-    // ALL TYPES OF VARIABLES
+    // // ALL TYPES OF VARIABLES
 
-    // integer
-    int age = 20;
-    int year = 3;
+    // printf("EXAMPLES USING DIFFERENT VARIABLES:\n\n");
 
-    printf("I'm %d years old\n", age);
-    printf("I'm a computer engineering student year %d\n", year);
+    // // integer
+    // int age = 20;
+    // int year = 3;
 
-    // float
 
-    float grade = 1.0;
-    float ppg = 20;
+    // printf("I'm %d years old\n", age); 
+    // printf("I'm a computer engineering student year %d\n", year);
 
-    printf("I just want to have a %.2f grade in Mixed Signals Lec this sem\n", grade);
-    printf("I used to average %f ppg 3 years ago\n", ppg);
+    // // float
 
-    // long float or doubles for high precision numbers
+    // float grade = 1.0;
+    // float ppg = 20;
 
-    double pi = 3.141592653589793;
-    double e = 2.718281828459045;
+    // printf("I just want to have a %.2f grade in Mixed Signals Lec this sem\n", grade);
+    // printf("I used to average %.1f ppg 3 years ago\n", ppg);
 
-    printf("The value of pi is %.15lf\n", pi);
-    printf("The value of euler is %.15lf\n", e);
+    // // long float or doubles for high precision numbers
 
-    //character
+    // double pi = 3.141592653589793;
+    // double e = 2.718281828459045;
 
-    char studentGrade = 'A';        // use single quotes
+    // printf("The value of pi is %.15lf\n", pi);
+    // printf("The value of euler is %.15lf\n", e);
 
-    printf("I used to be a straight %c grade student back in highschool.\n", studentGrade);
+    // //character
 
-    //string 
+    // char studentGrade = 'A';        // use single quotes
 
-    char team[] = "CEAT Men's Basketball";
+    // printf("I used to be a straight %c grade student back in highschool.\n", studentGrade);
 
-    printf("During my 2nd year college, I managed to play for %s team\n", team);
+    // //string 
 
-    //booleans
+    // char team[] = "CEAT Men's Basketball";
 
-    bool isPinoy = true;
+    // printf("During my 2nd year college, I managed to play for %s team\n", team);
 
-    if (isPinoy) {
-        printf("You are a stupih bih ahh pinoy nihh.\n");
-    } else {
-        printf("You are not a stupih bih ahh pinoy nihh.\n");
-    };
+    // //booleans
+
+    // bool isPinoy = true;
+
+    // if (isPinoy) {
+    //     printf("You are a stupih bih ahh pinoy nihh.\n");
+    // } else {
+    //     printf("You are not a stupih bih ahh pinoy nihh.\n");
+    // };
+    
+
+    // printf("\n");
+
+    // // VARIABLES FORMAT SPECIFICERS
+
+    // printf("EXAMPLES USING FORMAT SPECIFIERS:\n\n");
+
+
+    // // Integer
+
+    // int numWithMinimum = 2;
+    // int numWithMinimumAndZeros = 3;
+
+    // printf("This number leaves 2 spaces before it: ->%3d\n", numWithMinimum);
+    // printf("This number leaves 3 spaces before it with zeros: ->%04d\n", numWithMinimumAndZeros);
+
+    // // Floats
+
+    // float price1 = 10.99;
+    // float price2 = 3.49;
+    // float price3 = -79.99;
+
+    // printf("The price is $%+.2f\n", price1);
+    // printf("The price is $%+.2f\n", price2);
+    // printf("The price is $%+.2f\n", price3);
+    
+    
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';  // null terminator
+    char name[30] = "";
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);          // & means we'll stick a value in age variable
+
+    printf("Enter your gpa: ");
+    scanf("%f", &gpa);
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+
+    getchar();
+
+    printf("Enter your full name: ");
+    fgets(name, sizeof(name), stdin);       // file get string (fgets), the input has
+                                            // name = "James Sabio\n"
+    name[strlen(name) - 1] = '\0';          // name = "James Sabio\0"
+
+    printf("%s\n", name);
+    printf("%d\n", age);
+    printf("%.2f\n", gpa);
+    printf("%c\n", grade);
+
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+    name[strlen(name) - 1] = '\0';          // name = "James Sabio\0"
