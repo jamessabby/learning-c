7348feb695895036d9456809b92b6bc9f6cf095e
James Sabio <jamesjacobsabio232@gmail.com>
2025-12-24 10:19:20 +0800

    Learned math functions

---
+++ math_functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+  float num = -9.32;
+
+  // num = pow(num, 2);
+  // num = sqrt(num);
+  // num = round(num);
+  // num = floor(num);
+  // num = ceil(num);
+  // num = abs(num);
+
+  printf("%f", num);
+
+  return 0;
+}
